e48a526bf6c50032565b5b8a87118670b871be93
Wildyson Santos <wildyson.santos@gmail.com>
2022-04-19 18:27:24 -0300

    server

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +120,8 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    //'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
